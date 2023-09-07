ba1619e8a242692e5abdc892c890edd5b47451c9
Ngajimbajr <ngajimbajr@gmail.com>
2023-09-07 16:07:16 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 import sys
 from calculator_1 import add, sub, mul, div
 
@@ -7,14 +8,16 @@ def main():
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-        a, operator, b = map(str.strip, sys.argv[1:4])
+    a, operator, b = map(str.strip, sys.argv[1:4])
 
-        if operator not in '+-*/':
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
+    if operator not in '+-*/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-            operations = {'+': add, '-': sub, '*': mul, '/': div}
-            result = operations[operator](int(a), int(b))
-            print(f"{a} {operator} {b} = {result}")
-            if __name__ == "__main__":
-                main()
+    operations = {'+': add, '-': sub, '*': mul, '/': div}
+    result = operations[operator](int(a), int(b))
+    print(f"{a} {operator} {b} = {result}")
+
+if __name__ == "__main__":
+    main()
+
