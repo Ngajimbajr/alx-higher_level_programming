642bd67c5ba86bed72defa3a2b1cfe6552f21b2d
Ngajimbajr <ngajimbajr@gmail.com>
2023-09-11 11:52:40 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -12,9 +12,3 @@ def no_c(my_string):
             result += char
 
     return result
-
-if __name__ == "__main__":
-    print(no_c("Best School"))
-    print(no_c("Chicago"))
-    print(no_c("C is fun!"))
-
