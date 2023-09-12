bfd4f78efe31dd71ad1f5576cd7b159b3894ae49
Ngajimbajr <ngajimbajr@gmail.com>
2023-09-12 14:56:37 -0700

    100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,17 +1,22 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <Python.h>
+#include <time.h>
 
 /**
- * print_python_list_info - prints info about python lists
- * @p: address of pyobject struct
+ * print_python_list_info -  function that prints some basic
+ *							info about Python lists
+ * @p: python list
  */
-void print_python_list_info(PyObject *p)
-{
-	int i;
 
-	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
-	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
-	for (i = 0; i < Py_SIZE(p); i++)
-		printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+struct timespec;
+
+void print_python_list_info(PyObject *p) 
+{
+    int elem;
+
+    printf("[*] Size of the Python List = %ld\n", Py_SIZE(p));
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+    for (elem = 0; elem < Py_SIZE(p); elem++)
+        printf("Element %d: %s\n", elem, Py_TYPE(PyList_GetItem(p, elem))->tp_name);
 }
--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,60 @@
-#!/usr/bin/python3
-a = 89
-b = 10
-a,b = b,a
-print("a={:d} - b={:d}".format(a, b))
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * is_palindrome - a function that checks if a singly
+ * linked list is a palindrome.
+ * @head: a double pointer to the head of the list.
+ *
+ * Return: 1 if the linked list is a palindrome, 0 otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *ptr = *head;
+	listint_t *p1, *p2;
+	listint_t *slow = *head, *prev_slow = *head, *fast = *head;
+
+	if (head == NULL)
+		return (0);
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	while (fast && fast->next)
+	{
+		prev_slow = slow;
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+
+	p1 = prev_slow;
+
+	if (fast)
+		p2 = slow->next;
+	else
+		p2 = slow;
+	if ((p1->n) != (p2->n))
+		return (0);
+
+	ptr = *head;
+
+	while (1)
+	{
+		while (ptr->next->n != p1->n)
+		{
+			ptr = ptr->next;
+		}
+		p1 = ptr;
+		p2 = p2->next;
+
+		if (p1->n != p2->n)
+			break;
+		ptr = *head;
+
+		if (p1 = *head)
+			return (1);
+	}
+	return (0);
+}
