6da7df61f6443278b787a41052d5ca0a6e9d25ca
Ngajimbajr <ngajimbajr@gmail.com>
2023-10-04 15:32:46 -0700

    100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(my_list=[]):
-    my_list += ['Coding']
-    return ", ".join(my_list)
+def magic_string(H=[]):
+    H += ["BestSchool"]
+    return (", ".join(H))
